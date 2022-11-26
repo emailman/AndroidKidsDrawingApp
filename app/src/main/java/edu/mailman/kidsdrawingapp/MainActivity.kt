0a535a7148b7e39cfe29375ec33b277331592441
Eric Mailman <emailman@dtcc.edu>
2022-11-26 15:19:00 -0500

    Select background image

--- app/src/main/java/edu/mailman/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/edu/mailman/kidsdrawingapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package edu.mailman.kidsdrawingapp
 
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -18,7 +21,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawingView: DrawingView
     private lateinit var imageButtonCurrentPaint: ImageButton
 
-    val requestPermission: ActivityResultLauncher<Array<String>> =
+    private val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val imageBackground: ImageView = findViewById(R.id.iv_background)
+                imageBackground.setImageURI(result.data?.data)
+            }
+        }
+
+    private val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
             permissions ->
             permissions.entries.forEach {
@@ -30,6 +42,11 @@ class MainActivity : AppCompatActivity() {
                         "Permissions granted to read storage files",
                         Toast.LENGTH_LONG
                     ).show()
+
+                    val pickIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+
                 } else {
                     if (permissionName == android.Manifest.permission.READ_EXTERNAL_STORAGE) {
                         Toast.makeText(
